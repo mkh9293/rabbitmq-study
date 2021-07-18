42b96829a3201474635a210ea1a4b9e74b59fea5
mkh9293 <mkh9293@gmail.com>
2021-07-18 23:57:17 +0900

    first commit

---
+++ consumer/src/main/java/com/course/consumer/consumer/FixedRateConsumer.java
@@ -0,0 +1,21 @@
+package com.course.consumer.consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+@Service
+public class FixedRateConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FixedRateConsumer.class);
+
+    @RabbitListener(queues = "course.fixedrate", concurrency = "3-7")
+    public void listen(String message) throws InterruptedException {
+        TimeUnit.MICROSECONDS.sleep(ThreadLocalRandom.current().nextLong(1000, 2000));
+        LOG.info("{} : consuming {}", Thread.currentThread().getName(), message);
+    }
+}
---
+++ producer/src/main/java/com/course/producer/producer/FixedRateProducer.java
@@ -0,0 +1,26 @@
+package com.course.producer.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FixedRateProducer {
+
+    @Autowired
+    public RabbitTemplate rabbitTemplate;
+
+    private int i = 0;
+
+    public static final Logger LOG = LoggerFactory.getLogger(FixedRateProducer.class);
+
+    @Scheduled(fixedRate = 500)
+    public void sendMassage() {
+        i++;
+//        LOG.info("i is {}", i);
+        rabbitTemplate.convertAndSend("course.fixedrate", "fixed rate " + i);
+    }
+}
